ec3158298ab29f8b8f01cbd9c6d1647f9a659293
Ardackm <79649337+Ardackm@users.noreply.github.com>
2021-05-12 03:17:36 +0300

    main.css

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function randomString() {
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+	var string_length = 10;
+	var randomstring = '';
+	for (var i=0; i<string_length; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+	}
+	document.randform.randomfield.value = randomstring;
+}
+onload = randomString();
